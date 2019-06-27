9348f10d42e96ee1616c6ffe4c4324392e29cd47
taze-fullstack <taze@fullstack.ph>
2019-06-27 17:39:38 +0800

    update swiper dependency

--- src/containers/home.js
+++ src/containers/home.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
 import Swiper from 'react-id-swiper'
-import 'react-id-swiper/src/styles/scss/swiper.scss'
 
 import '../stylesheets/main.scss'
 
@@ -14,10 +13,6 @@ export default class Home extends Component {
 
         <Swiper>
           <div>Slide 1</div>
-          <div>Slide 2</div>
-          <div>Slide 3</div>
-          <div>Slide 4</div>
-          <div>Slide 5</div>
         </Swiper>
       </div>
     )
