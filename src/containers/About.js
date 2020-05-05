aa5396bf210dc11d2888a6c0942010bb9b7a69d8
taze-fullstack <taze@fullstack.ph>
2020-05-05 17:36:25 +0800

    add once checker

--- src/containers/About.js
+++ src/containers/About.js
@@ -1,7 +1,26 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React, {useEffect} from 'react';
+import {Link, useHistory} from 'react-router-dom';
 
 const About = () => {
+  let history = useHistory();
+
+  if (!sessionStorage.getItem('visitedUrls')) {
+    sessionStorage.setItem('visitedUrls', '[]');
+  } else {
+    let visitedUrls = JSON.parse(sessionStorage.getItem('visitedUrls'));
+    visitedUrls.push('/about');
+    sessionStorage.setItem('visitedUrls', JSON.stringify(visitedUrls));
+  }
+
+  useEffect(() => {
+    if (sessionStorage.getItem('visitedUrls')) {
+      let visitedUrls = JSON.parse(sessionStorage.getItem('visitedUrls'));
+      if (visitedUrls.indexOf('/about') !== -1) {
+        history.push('/');
+      }
+    }
+  }, []);
+
   return (
     <div className="about">
       <div>/about</div>
